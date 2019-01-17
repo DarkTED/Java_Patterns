8e7f47e376ffc9847bba25335fbc58a25a8405ba
Ilya Zlobin <ilya_zlobin@epam.com>
2019-01-17 13:32:15 +0300

    added Prototype, Singleton pattern

--- src/abstract_factory/example/Application.java
+++ src/abstract_factory/example/Application.java
@@ -1,9 +1,6 @@
 package abstract_factory.example;
 
-/**
- * Код, использующий фабрику, не волнует с какой конкретно фабрикой он работает.
- * Все получатели продуктов работают с продуктами через абстрактный интерфейс.
- */
+
 public class Application {
     private Button button;
     private Checkbox checkbox;
